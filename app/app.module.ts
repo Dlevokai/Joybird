704240fe8efba03da1f2ee160757164dcd555139
Your Name <D.iakovel@gmail.com>
2016-11-23 15:09:19 -0500

    carousel component added and working

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent }  from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JsonpModule } from '@angular/http';
+import { NgbdCarouselBasic } from './carousel/carousel.component';
 
 @NgModule({
-  imports:      [ BrowserModule, NgbModule.forRoot() ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, JsonpModule, NgbModule.forRoot() ],
+  declarations: [ AppComponent, NgbdCarouselBasic ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
